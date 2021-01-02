3734fa275e65d66dc0f855b95c06402921791418
M S <mixa.ru.ru@gmail.com>
2021-01-02 14:45:10 +0200

    footer updated with links

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,10 +5,10 @@
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
-      <span class="ml-3 text-xl">domain.com</span>
+      <span class="ml-3 text-xl">https://nuxt-i18n-blog.vercel.app/</span>
     </a>
-    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2021 domain.com —
-      <a href="https://ya.ru" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@ya</a>
+    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2021 https://github.com/davidgrzyb/tailwind-blog-template —
+      <a href="https://ya.ru" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">https://nuxt-i18n-blog.vercel.app</a>
     </p>
     <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
       <a class="text-gray-500">
