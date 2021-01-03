66cc91ee1f731e359dc8c2c47a9d2b912dab7607
M S <mixa.ru.ru@gmail.com>
2021-01-03 20:32:51 +0200

    date as filename in seeder

--- seeder/seed.js
+++ seeder/seed.js
@@ -2,11 +2,17 @@ var faker = require('faker')
 var fs = require('fs')
 var path = require('path')
 
-var dir = '../dist/md'
+var dir = '../content'
+
+var locale_dirs = ['ru', 'en', 'es']
+var n_posts = 10
+
+dir = path.join(__dirname, dir)
 
 if (fs.existsSync(dir)) {
     fs.rmdirSync(dir, { recursive: true });
 }
+
 
 fs.mkdir(dir, function(err) {
     if (err) {
@@ -14,26 +20,30 @@ fs.mkdir(dir, function(err) {
     } else {
       console.log("New directory successfully created.")
     }
-  })
+})
 
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < n_posts; i++) {
   generateMD()
 }
 
 function generateMD () {
-  var fileName = faker.lorem.word() + '-' + faker.lorem.word()
-  var fileContents = `---
+    const d = faker.date.past()
+    const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+    const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
+    const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+
+    var fileContents = `---
 title: "${faker.lorem.words()}"
 date: "${faker.date.past()}"
 ---
-${faker.lorem.sentence()}
-`
-  var outputPath = path.join(__dirname, dir, `${fileName}.md`)
+${faker.lorem.sentence()}`
 
-  fs.writeFile(outputPath, fileContents, function (err) {
-    if (err) {
-      return console.log(err)
-    }
+    var outputPath = path.join(dir, `${da}-${mo}-${ye}.md`)
+
+    fs.writeFile(outputPath, fileContents, function (err) {
+        if (err) {
+        return console.log(err)
+        }
     console.log(outputPath + ' file generated')
   })
 }
