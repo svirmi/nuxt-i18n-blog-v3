5e84a4ca3b76b6782656bb84573891df7b751e9b
M S <mixa.ru.ru@gmail.com>
2021-01-03 17:12:25 +0200

    unused code removed

--- components/Header.vue
+++ components/Header.vue
@@ -19,7 +19,7 @@
 
     <!-- Topic Nav -->
     <nav class="w-full py-4 border-t border-b bg-gray-100" x-data="{ open: false }">
-        <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+        <div class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
             <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
                 <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a>
                 <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Automotive</a>
--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/i18n'],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
