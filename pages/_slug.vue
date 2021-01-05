34abf4c1d7fcd1fa9dab440debbc546a6a5c02e2
M S <mixa.ru.ru@gmail.com>
2021-01-05 20:51:07 +0200

    typo

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -6,7 +6,7 @@
       <div class="bg-white flex flex-col justify-start p-6">
         <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
         <h1 class="text-3xl font-bold pb-4">{{ post.title }}</h1>
-        <p href="#" class="text-sm pb-8">Published on {{ post.date }}</p>  
+        <p class="text-sm pb-8">Published on {{ post.date }}</p>  
 
         <nuxt-content :document="post"></nuxt-content>
       </div>
